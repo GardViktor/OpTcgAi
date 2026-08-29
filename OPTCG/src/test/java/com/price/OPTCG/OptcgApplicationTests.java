d2504a5bab7c076e94ddbca388e46d022d8f065f
Edgard <edgardvictorlol@gmail.com>
2026-08-29 11:48:31 -0300

    first commit

---
+++ OPTCG/src/test/java/com/price/OPTCG/OptcgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.price.OPTCG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
